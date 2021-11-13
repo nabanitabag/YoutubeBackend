3a7a2a6dba99b58165996059fc6ca3ac7e0368bd
Nabanita Bag <ashley.makruz@gmail.com>
2021-11-13 18:37:03 +0530

    cookie-parser add

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 const secrets = require("./secrets");
 
 const logger = require("morgan");
@@ -24,8 +27,18 @@ mongoose
     }
   });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "public")));
+  
+  app.use("/", indexRouter);
+  
+  // Catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
 
 // Error handler
 app.use((err, req, res) => {

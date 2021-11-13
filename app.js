eb84fe9f69902c5d69d966463ef7f78fca7331eb
Nabanita Bag <ashley.makruz@gmail.com>
2021-11-13 18:00:21 +0530

    modify package.json

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const secrets = require("./secrets");
+
+const logger = require("morgan");
+
+const initFetchVideoJob = require("./fetchcron");
+const indexRouter = require("./index");
+
 const app = express();
 
 mongoose

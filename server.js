57be45ed2e5af97bde1c6e17c63bf71060522a46
Nabanita Bag <ashley.makruz@gmail.com>
2021-11-13 18:36:00 +0530

    add indexRouter

--- server.js
+++ server.js
@@ -9,14 +9,8 @@
  const port = normalizePort(process.env.PORT || '3000');
  app.set('port', port);
  
- /**
-  * Create HTTP server.
-  */
  const server = http.createServer(app);
  
- /**
-  * Listen on provided port, on all network interfaces.
-  */
  server.listen(port);
  server.on('error', onError);
  server.on('listening', onListening);
